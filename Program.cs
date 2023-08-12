2cb23a1bbd5034f6758ac6821725a0728a494a93
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2023-08-11 22:27:30 -0300

    Exercício do Haikai

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Nos dias quotidianos");
+Thread.Sleep(1000);
+Console.WriteLine("É que se passam");
+Thread.Sleep(1000);
+Console.WriteLine("Os anos");
+Thread.Sleep(1000);
+Console.WriteLine ("                Haikai de Millôr Fernandes");
+Thread.Sleep(1000);
